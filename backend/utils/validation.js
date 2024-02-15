21f7cbe7fcd953fd8b8e982f7847c384ede513a4
Asazay Luvaiku <AsazayLuvaiku@gmail.com>
2024-02-15 01:31:25 -0700

    Added User signup functionality

--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -18,6 +18,26 @@ const handleValidationErrors = async (req, res, next) => {
   next();
 };
 
+const handleExistsErrors = async (req, res, next) => {
+  const existsErrors = validationResult(req);
+
+  if(!existsErrors.isEmpty()){
+    const errors = {};
+
+    existsErrors.array().forEach(error => errors[error.path] = error.msg);
+
+    const err = new Error('User already exists');
+    err.errors = errors;
+    err.status = 500;
+    err.title = "User already exists";
+    next(err);
+  }
+
+  next();
+};
+
+
+
 module.exports = {
-  handleValidationErrors
+  handleValidationErrors, handleExistsErrors
 }
