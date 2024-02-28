a27fb8eea073e5134603fb82837869121dd8bea1
Asazay Luvaiku <AsazayLuvaiku@gmail.com>
2024-02-27 20:36:15 -0700

    Completed Add Image to a Review by Id

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -31,4 +31,45 @@ router.get('/current', checkAuthorization, async (req, res, next) => {
   res.json(allReviews)
 });
 
+router.post('/:reviewId/images', checkAuthorization, async (req, res, next) => {
+  const {reviewId} = req.params;
+  const {url} = req.body;
+
+  let review = await Review.findByPk(reviewId);
+
+  if(!review){
+    const err = new Error("Review couldn't be found");
+    err.status = 404;
+    err.title = "Couldn't find review";
+    err.message = "Review couldn't be foud";
+    return next(err);
+  }
+
+  const reviewImages = await review.getReviewImages();
+
+  if(reviewImages.length >= 10){
+    const err = new Error("Maximum number of images for this resource was reached");
+    err.status = 403;
+    err.title = "Maximum number of images reached";
+    err.message = "Maximum number of images for this resource reached";
+    return next(err);
+  }
+
+  if(!checkAuth(req.user.id, review.userId)){
+    const err = new Error('Authorization required');
+    err.title = 'Authorization required';
+    err.errors = { message: 'Authorization required' };
+    err.status = 403;
+    res.status(403);
+    return next(err);
+  }
+
+  const newRevImg = await ReviewImage.create({
+    reviewId: review.id,
+    url: url
+  });
+
+  res.json(newRevImg);
+})
+
 module.exports = router;
