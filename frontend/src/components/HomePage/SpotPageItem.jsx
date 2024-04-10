3621894a6180069240eed30226f5b4586eb214da
Asazay Luvaiku <AsazayLuvaiku@gmail.com>
2024-04-09 22:15:44 -0700

    Made css changes to index page based off requirements as well as changed a few back end files to better present page

--- frontend/src/components/HomePage/SpotPageItem.jsx
+++ frontend/src/components/HomePage/SpotPageItem.jsx
@@ -1,15 +1,23 @@
 import { NavLink } from "react-router-dom";
 
 const SpotPageItem = ({spot}) => {
+    let newAvgRating = spot.avgRating;
+
+    if(!newAvgRating) newAvgRating = 0.0;
+
     return (
-        <div id={spot.id}>
+        <div id={spot.id} className="toolTip">
+            <span className="toolTipText">{spot.name}</span>
            <NavLink to={`/spots/${spot.id}`}>
            <div>
                 <img width={220} height={220} src={spot.previewImage} alt={spot.previewImage}/>
             </div>
             <div>
+                <div id="locationStars">
                 <p>{`${spot.city}, ${spot.state}`}</p>
-                <p id="boldPrice">{`$${spot.price}`}<span> night</span></p>
+                <p>⭐{newAvgRating.toFixed(1)}</p>
+                </div>
+                <p><span id='boldPrice'>{`$${spot.price}`}</span> night</p>
             </div>
            </NavLink>
         </div>
